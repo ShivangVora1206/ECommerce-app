dc11bcd63ab7b792b9378c899a37b4d8b4b4ec75
Shivang Vora <69647458+ShivangVora1206@users.noreply.github.com>
2023-08-24 00:09:26 +0530

    Removed mongo uri from project

--- database/init.js
+++ database/init.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = function (){
-    mongoose.connect("mongodb+srv://todo-app:todo-app1@cluster0.l15a2yt.mongodb.net/e-commerce?retryWrites=true&w=majority")
+    mongoose.connect("URI")
 .then(()=>{
     console.log("connected to db");
 }).catch(()=>{
